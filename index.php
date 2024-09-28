ec8c75c5f49003a37b0d2eff4cfdaa3d9eca025d
unknown <utkarsh.mudgal123si@gmail.com>
2024-09-28 19:26:41 +0530

    Set up basic contact form in add_contact view

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'includes/functions.php';
-
-// Fetch all contacts from the database
-$contacts = selectAll('contacts');
-
-// Include the view for displaying contacts
-include 'views/index.view.php';
